83f1f3d5e6dfcafc197fdd9b3c522b67b86733e5
Mchzks <dvillalobos@ammper.com>
2025-03-19 22:54:32 -0500

    comments remove

--- createEmotionCache.js
+++ createEmotionCache.js
@@ -1,7 +1,5 @@
-// createEmotionCache.js
 import createCache from '@emotion/cache';
 
-// Client-side cache, shared for the whole session of the user in the browser.
 export default function createEmotionCache() {
   return createCache({ key: 'css', prepend: true });
 }
